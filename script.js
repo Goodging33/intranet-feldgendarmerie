fa7be4ce51c54c431efe4e8b80cd6b7cf9481591
intranet-feldgendarmerie <goodging33@gmail.com>
2026-02-03 12:42:38 +0100

    Remove alert from script.js
    Removed alert statement from script.js.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-alert("script.js chargé");
-
 const SUPABASE_URL = "https://bdkzvkdqznkjkvpxwmqg.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJka3p2a2Rxem5ramt2cHh3bXFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwNDkwNzksImV4cCI6MjA4NTYyNTA3OX0.WPqm4torQsQjDcERoZtTsaGexR4V2GEpn9GtIMelALM";
 
