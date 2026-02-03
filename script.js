3a16095f9d96d2b35481f705d6b7e1b73053fcba
intranet-feldgendarmerie <goodging33@gmail.com>
2026-02-03 12:01:43 +0100

    Add alert for script.js loading

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+alert("script.js chargé");
+
 const SUPABASE_URL = "sb_publishable_hkqe9roWQ7f-HQp9S9FEzg_i-pmZ6sb";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJka3p2a2Rxem5ramt2cHh3bXFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwNDkwNzksImV4cCI6MjA4NTYyNTA3OX0.WPqm4torQsQjDcERoZtTsaGexR4V2GEpn9GtIMelALM";
 
